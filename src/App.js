f3590772f963cc15c41d80602fbbba71597e09a7
Bruno Ardel <6134807-brunoardel@users.noreply.gitlab.com>
2021-05-28 00:42:56 -0300

    Configuração gh-pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Principal from './Principal.jsx';
 
 import './App.css';
 
 export default class App extends Component {
-  	render() {
-		return (
-			<Router>
-				<div>
-					<Route path="/categoria/:categoria" component={Principal}></Route>
-					<Route path="/" exact component={Principal}></Route>
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/categoria/:categoria" component={Principal}></Route>
+          <Route path="/" exact component={Principal}></Route>
+        </div>
+      </Router>
+    );
+  }
 }
-
--- src/Principal.jsx
+++ src/Principal.jsx
@@ -4,28 +4,25 @@ import Menu from './Componentes/Menu/Menu';
 import Noticias from './Componentes/Noticias/Noticias';
 
 export default function Principal(props) {
-	const categorias = [
-		{'url': 'general', 'nome': 'Geral'}, 
-		{'url': 'technology', 'nome': 'Tecnologia'},
-		{'url': 'health', 'nome': 'Saúde'},
-		{'url': 'business', 'nome': 'Negócios'},
-		{'url': 'entertainment', 'nome': 'Entretenimento'},
-		{'url': 'sports', 'nome': 'Esportes'}
-	];
+  const categorias = [
+    { url: 'general', nome: 'Geral' },
+    { url: 'technology', nome: 'Tecnologia' },
+    { url: 'health', nome: 'Saúde' },
+    { url: 'business', nome: 'Negócios' },
+    { url: 'entertainment', nome: 'Entretenimento' },
+    { url: 'sports', nome: 'Esportes' },
+  ];
 
-	let categoriaAtiva = 'general';
-	if(typeof props.match.params.categoria !== 'undefined'){
-		categoriaAtiva = props.match.params.categoria;
-	}
+  let categoriaAtiva = 'general';
+  if (typeof props.match.params.categoria !== 'undefined') {
+    categoriaAtiva = props.match.params.categoria;
+  }
 
-	return (
-		<div className="container">
-			<Topo titulo={'Últimas Notícias'} logo={'/logo.png'} />
-			<Menu categorias={categorias} categoriaAtiva={categoriaAtiva} />
-			<Noticias categoriaAtiva={categoriaAtiva} />
-		</div>
-	);
+  return (
+    <div className="container">
+      <Topo titulo={'Últimas Notícias'} logo={'/logo.png'} />
+      <Menu categorias={categorias} categoriaAtiva={categoriaAtiva} />
+      <Noticias categoriaAtiva={categoriaAtiva} />
+    </div>
+  );
 }
-
-
-
