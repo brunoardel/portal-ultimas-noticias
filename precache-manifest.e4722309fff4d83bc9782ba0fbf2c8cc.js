446f2d8820f34f9b090452931ccc8c543ceb236a
Bruno Ardel <6134807-brunoardel@users.noreply.gitlab.com>
2021-05-28 02:46:04 -0300

    Updates

---
+++ precache-manifest.e4722309fff4d83bc9782ba0fbf2c8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0ea01bdc7c28a69c11f79d48e653ff",
+    "url": "/portal-ultimas-noticias/index.html"
+  },
+  {
+    "revision": "6dc77aeec672c501a894",
+    "url": "/portal-ultimas-noticias/static/css/main.84614bb8.chunk.css"
+  },
+  {
+    "revision": "74ade36d47eee5604d6a",
+    "url": "/portal-ultimas-noticias/static/js/2.bce370a1.chunk.js"
+  },
+  {
+    "revision": "6dc77aeec672c501a894",
+    "url": "/portal-ultimas-noticias/static/js/main.17d2a997.chunk.js"
+  },
+  {
+    "revision": "e35bd9666eee7e257c54",
+    "url": "/portal-ultimas-noticias/static/js/runtime-main.9d33e3d9.js"
+  }
+]);
